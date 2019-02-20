6e3a6af3ea7dde4805fe8c83899035bb0422ad44
ashraf1597 <47810934+ashraf1597@users.noreply.github.com>
2019-02-20 21:09:52 +0530

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yl^2a4pop&kr$94@udh54_p5hsia9b)951#ankc(6ig&ll=#0e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
