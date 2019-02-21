8dfd0b173c167843d5e9b2208db7a66574ca779f
ashraf <ashraf.ali1597@gmail.com>
2019-02-21 08:06:29 +0530

    Added additional features

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,9 @@ from django.contrib.auth.decorators import login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' :posts})
+    query=request.GET.get("q")
+    if query:
+        queryset_list=queryset_list.filter(title__icontains=query)
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
